dd1627060404648256da8b55b17c5d7807ada0e0
developershubhamm <mahajanshubham411@gmail.com>
2024-06-25 10:57:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+function turnOn(){
+ document.getElementById("LightON").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png"
+    document.getElementById("CatOn").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png"
+    document.getElementById("text").textContent="Light is ON"
+}
+function turnOff(){
+   
+
+
+    document.getElementById("LightON").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png"
+document.getElementById("CatOn").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png"
+document.getElementById("text").textContent= "Light is off"
+    
+
+}
